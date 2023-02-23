19cc99ede1e3abc8c0ca2de059e320ff38df1cf1
Henry Donahue <126025438+hdonahue27@users.noreply.github.com>
2023-02-23 15:59:14 +0000

    Minor code organization improvements

--- PyDice.py
+++ PyDice.py
@@ -1,24 +1,31 @@
 import random
 
+# main menu
+def menu():
+  print("Welcome to PyDice!")
+  print("S: Start PyDice")
+  print("Q: Quit PyDice")
+  print("I: View PyDice info")
+  menuInput = input("Choose an option\n")
+  if menuInput == "s" or menuInput == "S":
+    start()
+  elif menuInput == "q" or menuInput == "Q":
+    quit()
+  elif menuInput == "i" or menuInput == "I":
+    info()
+
+# dice/side select
 def start():
     global diceNum
-    diceNum = input("Choose the amount of dice you would like to roll\n")
+    diceNum = input("\nChoose the amount of dice you would like to roll\n")
     diceNum = int(diceNum)
     global sideNum
-    sideNum = input("Choose the amount of sides that each die will have\n")
+    sideNum = input("\nChoose the amount of sides that each die will have\n")
     sideNum = int(sideNum)
     print("\nTo view a list of commands, type C and press enter.")
-    rollPrompt(diceNum=diceNum, sideNum=sideNum)
+    rollPrompt(diceNum=diceNum, sideNum=sideNum) 
 
-def cmds():
-  print("\nHere are the commands you can use:\n")
-  print("n: Change number of dice rolled")
-  print("s: Change number of sides on the dice")
-  print("d: Deletes all previous roles and resets the dice settings")
-  print("m: Return to main menu")
-  print("q: Terminate the program")
-  rollPrompt(diceNum, sideNum)    
-
+# dice engine and value changer
 def rollPrompt(diceNum, sideNum):
   rollPromptInput = input("\nPress enter to roll, or type a command\n")
   if rollPromptInput == "m" or rollPromptInput == "M":
@@ -42,24 +49,22 @@ def rollPrompt(diceNum, sideNum):
     print(str(random.randint(0,sideNum)))
   rollPrompt(diceNum, sideNum)
 
+# info page
 def info():
-  print("pyDice 1.1.0")
+  print("\nPyDice 1.1.1")
   print("Created by Henry Donahue")
-  print("https://www.github.com/hdonahue27")
-  infoInput = input("Press enter to return to main menu\n")
+  print("https://www.github.com/hdonahue27/PyDice")
+  infoInput = input("\nPress enter to return to main menu\n")
   menu()
 
-def menu():
-  print("Welcome to pyDice!")
-  print("S: Start pyDice")
-  print("Q: Quit pyDice")
-  print("I: View pyDice info")
-  menuInput = input("Choose an option\n")
-  if menuInput == "s" or menuInput == "S":
-    start()
-  if menuInput == "q" or "Q":
-    quit()
-  if menuInput == "i" or "I":
-    info()
+# command list
+def cmds():
+  print("\nHere are the commands you can use:\n")
+  print("n: Change number of dice rolled")
+  print("s: Change number of sides on the dice")
+  print("d: Deletes all previous roles and resets the dice settings")
+  print("m: Return to main menu")
+  print("q: Terminate the program")
+  rollPrompt(diceNum, sideNum)
 
 menu()
